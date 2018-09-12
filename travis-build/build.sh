b691158c37167d8b9b0e973117dd6ed09c147202
HoogendoornM <Hoog0095@hz.nl>
2018-09-12 18:24:29 +0200

    Testing build of exe

--- travis-build/build.sh
+++ travis-build/build.sh
@@ -1,37 +1,20 @@
 #! /bin/sh
 
-PROJECT_PATH=$(pwd)/$UNITY_PROJECT_PATH
-UNITY_BUILD_DIR=$(pwd)/Build
-LOG_FILE=$UNITY_BUILD_DIR/unity-win.log
+project="<YOUR PROJECT NAME HERE>"
 
-
-ERROR_CODE=0
-echo "Items in project path ($project_path):"
-ls "$project_path"
-
-
-echo "Building project for Windows..."
-mkdir $UNITY_BUILD_DIR
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -batchmode \
-  -nographics \
-  -silent-crashes \
-  -logFile \
-  -projectPath "$PROJECT_PATH" \
-  -buildWindows64Player  "$(pwd)/build/win/ci-build.exe" \
-  -quit \
-  | tee "$LOG_FILE"
+echo "Attempting to build $project for Windows"
+/Applications/Unity/Unity.app/Contents/MacOS/Unity 
+  -batchmode 
+  -nographics 
+  -silent-crashes 
+  -logFile $(pwd)/unity.log 
+  -projectPath $(pwd) 
+  -buildWindowsPlayer "$(pwd)/Build/windows/$project.exe" 
+  -quit
   
-if [ $? = 0 ] ; then
-  echo "Building Windows exe completed successfully."
-  ERROR_CODE=0
-else
-  echo "Building Windows exe failed. Exited with $?."
-  ERROR_CODE=1
-fi
+  echo 'Logs from build'
+cat $(pwd)/unity.log
 
-#echo 'Build logs:'
-#cat $LOG_FILE
 
-echo "Finishing with code $ERROR_CODE"
-exit $ERROR_CODE
+echo 'Attempting to zip builds'
+zip -r $(pwd)/Build/windows.zip $(pwd)/Build/windows/
