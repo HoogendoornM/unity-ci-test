c359bb04a129e8dc206046f84146c68eeb13348c
HoogendoornM <hoog0095@hz.nl>
2018-09-12 15:53:46 +0200

    Packing file

--- Scripts/post_build.sh
+++ Scripts/post_build.sh
@@ -1,23 +1,57 @@
 #! /bin/sh
 
-winname="${UNITYCI_PROJECT_NAME}_win.tar.gz"
-osxname="${UNITYCI_PROJECT_NAME}_osx.tar.gz"
+PROJECT_PATH=$(pwd)/$UNITY_PROJECT_PATH
+UNITY_BUILD_DIR=$(pwd)/Build
+LOG_FILE=$UNITY_BUILD_DIR/unity-win.log
+EXPORT_PATH=$(pwd)/$PROJECT_NAME-v"$TRAVIS_TAG"-b"$TRAVIS_BUILD_NUMBER".unitypackage
+RELEASE_DIRECTORY=./release
 
-# set file permissions so builds can run out of the box
-for infile in `find $(pwd)/Build | grep -E '\.exe$|\.dll$'`; do
-	chmod 755 $infile
-done
 
-# tar and zip the build folders
-tar -C "$(pwd)/Build" -czvf $winname "windows"
-tar -C "$(pwd)/Build" -czvf $osxname "osx"
+ERROR_CODE=0
 
-# upload the tarballs to the server for storage
-scp -i "${UPLOAD_KEYPATH}" \
-	-o stricthostkeychecking=no \
-	-o loglevel=quiet \
-	$winname "${UPLOAD_USER}@${UPLOAD_HOST}:${UPLOAD_PATH}" > /dev/null 2>&1
-scp -i "${UPLOAD_KEYPATH}" \
-	-o stricthostkeychecking=no \
-	-o loglevel=quiet \
-	$osxname "${UPLOAD_USER}@${UPLOAD_HOST}:${UPLOAD_PATH}" > /dev/null 2>&1
+echo "Creating package at=$EXPORT_PATH"
+mkdir $UNITY_BUILD_DIR
+/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+  -batchmode \
+  -nographics \
+  -silent-crashes \
+  -logFile \
+  -projectPath "$PROJECT_PATH" \
+  -exportPackage "Assets" "$EXPORT_PATH" \
+  -quit \
+  | tee "$LOG_FILE"
+  
+if [ $? = 0 ] ; then
+	echo "Created package successfully."
+	ERROR_CODE=0
+	
+	echo "Packaging unity package into release..."
+	#Preprare release unity package by packing into ZIP
+	RELEASE_ZIP_FILE=$RELEASE_DIRECTORY/$PROJECT_NAME-v$TRAVIS_TAG.zip
+
+	mkdir -p $RELEASE_DIRECTORY
+
+	echo "Preparing release for version: $TRAVIS_TAG"
+	cp "$EXPORT_PATH" "$RELEASE_DIRECTORY/"`basename "$EXPORT_PATH"`
+	cp "./README.md" "$RELEASE_DIRECTORY"
+	cp "./LICENSE" "$RELEASE_DIRECTORY"
+
+	echo "Files in release directory:"
+	ls $RELEASE_DIRECTORY
+
+	zip -6 -r $RELEASE_ZIP_FILE $RELEASE_DIRECTORY
+
+	echo "Release zip package ready. Zipinfo:"
+	zipinfo $RELEASE_ZIP_FILE
+	
+else
+	echo "Creating package failed. Exited with $?."
+	ls
+	ERROR_CODE=1
+fi
+
+#echo 'Build logs:'
+#cat $LOG_FILE
+
+echo "Export finished with code $ERROR_CODE"
+exit $ERROR_CODE
