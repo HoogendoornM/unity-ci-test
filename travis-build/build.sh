c071c297d2562b5a7ced566dd163c0a68db91f9b
HoogendoornM <Hoog0095@hz.nl>
2018-09-13 18:14:49 +0200

    Trying zip without parent directory

--- travis-build/build.sh
+++ travis-build/build.sh
@@ -31,7 +31,7 @@ else
 fi
 
 echo 'Attempting to zip builds'
-cd $(pwd) ; zip -r /Build/windows.zip . * ; cd ..
+cd $(pwd) ; zip -r /Build/windows.zip * ; cd ..
 
 #echo 'Build logs:'
 #cat $LOG_FILE
